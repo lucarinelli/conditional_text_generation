baa19204d54c6489947354085b2f40ba38a12302
Luca Rinelli <rinelliiluca@gmail.com>
2021-05-30 09:59:00 +0000

    Add gpt2 notebook using py files and small fix

--- src/experiment_parameters.py
+++ src/experiment_parameters.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 
 class ExperimentParameters:
-    __init__(run_name="unnamed_run", use_control_codes=True, control_codes_type="special_token", use_supercategories=True, use_categories=False, control_codes_powerset=True, max_control_codes_per_caption=3, limited_run=True, max_train_set_len=5000, max_val_set_len=1000, model="gpt2", chunk_size_json_mp="500", force_dataset_update=False, training_args=None):
+    def __init__(self, run_name="unnamed_run", use_control_codes=True, control_codes_type="special_token", use_supercategories=True, use_categories=False, control_codes_powerset=True, max_control_codes_per_caption=3, limited_run=True, max_train_set_len=5000, max_val_set_len=1000, model="gpt2", chunk_size_json_mp=500, force_dataset_update=False, training_args=None):
         self.run_name=run_name
         self.use_control_codes=use_control_codes
         self.control_codes_type=control_codes_type
